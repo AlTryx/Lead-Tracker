fdf7ac594be8278826011946663a64ee2da109d7
Aleksandar Stanchev <altryx15@gmail.com>
2024-03-15 22:54:29 +0200

    Added a delete button

--- chrome_extension.js
+++ chrome_extension.js
@@ -1,29 +1,41 @@
 let myLeads = []
 const inputBtn = document.getElementById("input-btn")
-
 const inputEl = document.getElementById("input-el")
-
 const ulEl = document.getElementById("ul-el")
 
+let deleteBtn = document.getElementById("delete-btn")
 
-inputBtn.addEventListener("click", function() {
-myLeads.push(inputEl.value)
-inputEl.value = ""
-renderLeads()
-}
-)
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
-
-function renderLeads(){
-let listItems = ""
-
-for (let i = 0; i < myLeads.length; i++){
-    
-   listItems += `<li> 
-    <a href = '${myLeads[i]}', target = '_blank'>  ${myLeads[i]}  </a>
-    </li>`
-   
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage          // ensure-va kogato refreshna stranicata da se zapazqt linkovete(leadovete)
+  renderLeads()
 }
 
-ulEl.innerHTML = listItems
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value)
+  inputEl.value = ""
+
+
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
+
+  renderLeads()
+
+  console.log(localStorage.getItem("myLeads"))
+})
+
+
+function renderLeads() {
+  let listItems = ""
+
+  for (let i = 0; i < myLeads.length; i++) {
+
+    listItems += `<li> 
+      <a href = '${myLeads[i]}', target = '_blank'>  ${myLeads[i]}  </a>
+      </li>`
+
+  }
+
+  ulEl.innerHTML = listItems
 }
