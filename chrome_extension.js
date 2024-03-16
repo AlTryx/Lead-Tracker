66eb43d159097f62ab40f55924953e49e4560987
Aleksandar Stanchev <altryx15@gmail.com>
2024-03-16 13:57:24 +0200

    Added a "SAVE TAB" button

--- chrome_extension.js
+++ chrome_extension.js
@@ -1,41 +1,58 @@
 let myLeads = []
+let oldLeads = []
 const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
-
-let deleteBtn = document.getElementById("delete-btn")
-
+const deleteBtn = document.getElementById("delete-btn")
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const tabBtn = document.getElementById("tab-btn")
+
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage          // ensure-va kogato refreshna stranicata da se zapazqt linkovete(leadovete)
-  renderLeads()
+  render(myLeads)
 }
 
+tabBtn.addEventListener("click", function(){
+  
+  //chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+  //})
 
-inputBtn.addEventListener("click", function () {
-  myLeads.push(inputEl.value)
-  inputEl.value = ""
-
-
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+  myLeads.push(tabs[0].url)
   localStorage.setItem("myLeads", JSON.stringify(myLeads))
-
-  renderLeads()
-
-  console.log(localStorage.getItem("myLeads"))
+  render(myLeads)
+  })
+  
+ 
 })
 
 
-function renderLeads() {
+function render(leads) {
   let listItems = ""
 
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
 
     listItems += `<li> 
-      <a href = '${myLeads[i]}', target = '_blank'>  ${myLeads[i]}  </a>
+      <a href = '${leads[i]}', target = '_blank'>  ${leads[i]}  </a>
       </li>`
 
   }
 
   ulEl.innerHTML = listItems
 }
+
+deleteBtn.addEventListener("dblclick", function(){
+  localStorage.clear();
+  myLeads = []
+  render(myLeads)
+})
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value)
+  inputEl.value = ""
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
+  render(myLeads)
+})
+
+
